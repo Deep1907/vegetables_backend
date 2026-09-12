5700fb6d4933d9198c1da91deb92fb863e85dbf3
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-12 23:03:16 +0530

    again changes

--- controller/userControllers.jsx
+++ controller/userControllers.jsx
@@ -1,18 +1,21 @@
 const express = require("express")
 const userModel = require("../models/Users.jsx")
 
-const addUser = async (req,res) =>{
-    try{
-        const {username,email,password} = req.body
-        const userExists = await userModel.find(email)
-        if(userExists){
-            res.status(400).json({success:false,message:"User Already Exists"})
-        }
-        const user = await userModel.create({username,email,password})
-        res.status(200).json({success:true,message:"User Added Successfully"})
+const addUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body
 
-    }catch(err){
+        const userExists = await userModel.findOne({ email })
+        if (userExists) {
+            return res.status(400).json({ success: false, message: "User Already Exists" })
+        }
+
+        const user = await userModel.create({ username, email, password })
+        return res.status(200).json({ success: true, message: "User Added Successfully" })
+
+    } catch (err) {
         console.log(err)
+        return res.status(500).json({ success: false, message: "Server Error" })
     }
 }
 
