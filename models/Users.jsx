341a97a273467f83c022de307192af25163e33fd
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-12 21:41:36 +0530

    changed again

--- models/Users.jsx
+++ models/Users.jsx
@@ -15,6 +15,6 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const userModel = mongoose.model(users,"UserSchema")
+const userModel = mongoose.model("users",UserSchema)
 
 module.exports = userModel;
