25b901d7b4f106a9a4fbbc898cf2a0eedcba48f4
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-13 18:40:39 +0530

    backend changed

---
+++ middleware/isAuth.jsx
@@ -0,0 +1,47 @@
+const userModel = require("../models/Users.jsx");
+const jwt = require("jsonwebtoken");
+
+export const isAuthenticated = async (req, res, next) => {
+
+    try {
+
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({
+                success: false,
+                message: "Token Invalid"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decodeToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        const userId = decodeToken.userId;
+
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+
+        return res.status(401).json({
+            success: false,
+            message: "Invalid or expired token"
+        });
+
+    }
+};
