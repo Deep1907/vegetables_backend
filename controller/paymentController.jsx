8b551af09f91075d7e3923556ec05967b8cb901f
Deep1907 <deepakkguptaa343@gmail.com>
2026-09-16 23:32:54 +0530

    created order for payment razorpay

--- controller/paymentController.jsx
+++ controller/paymentController.jsx
@@ -1,7 +1,22 @@
 const express = require("express")
+const razorpayInstance = require("../utils/razorpay.js")
 
 const paymentController = async (req,res) =>{
     try{
+
+        const order = await razorpayInstance.orders.create({
+            "amount":50000,
+            "currency":"INR",
+            "receipt":"receipt#1",
+            "notes":{
+                username : req.user.username,
+                email : req.user.email
+            }
+        })
+
+        console.log("Order",order)
+
+        res.json({order})
 
     }catch(err){
         console.log(err)
--- router/paymentRoute.js
+++ router/paymentRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const paymentController = require("../controller/paymentController.jsx")
+const {isAuthenticated} = require("../middleware/isAuth.jsx")
 
 const paymentRouter = express.Router()
 
-paymentRouter.post("/create",paymentController)
+paymentRouter.post("/create",isAuthenticated,paymentController)
 
 module.exports = paymentRouter;
